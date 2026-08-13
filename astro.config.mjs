7a46252723caa8d423409bdccd56f52d4bb936ad
Micha Buchholz <mail@lean-stack.rocks>
2026-08-13 12:11:15 +0200

    chore: Delete unpublished pages and the now obsolete solid integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind()]
 });
